11c81bc4217342ca8edbc1061eb57769a6df0781
crystal-luong <balb2k2@gmail.com>
2023-01-28 03:53:46 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+const navbarEl = document.querySelector(".navbar");
+const bottomEl = document.querySelector(".bottom-container");
+const hamburgerEl = document.getElementById('hamburger');
+const navbarUlEl = document.querySelector('.navbarUl');
+const blogEl = document.getElementById('blog');
+const homeEl = document.getElementById('home');
+const topContainterEl = document.getElementById('top-container');
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > bottomEl.offsetTop - 50 -navbarEl.offsetHeight) {
+        homeEl.classList.remove('active');
+        hamburgerEl.classList.add('active');
+        blogEl.classList.add('actived');
+        navbarUlEl.classList.add('actived');
+
+    }
+    else {
+        homeEl.classList.add('active');
+        hamburgerEl.classList.remove('active');
+        blogEl.classList.remove('actived');
+        navbarUlEl.classList.remove('actived');
+    }
+})
+
+hamburgerEl.addEventListener('click', () => {
+ if(navbarUlEl.clientHeight === 0) {
+    navbarUlEl.classList.add('active');
+ }
+ else {
+    navbarUlEl.classList.remove('active');
+ }
+ 
+})
+
+navbarUlEl.addEventListener('click', () => {
+    navbarUlEl.classList.remove('active');
+})
